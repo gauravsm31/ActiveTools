5485c21c88ea14917a2a08f3ac11359d2bfe2a38
gauravsm31 <gauravsm@uw.edu>
2019-09-25 17:45:24 -0700

    batch processing 13

--- library_count_map.py
+++ library_count_map.py
@@ -89,7 +89,7 @@ class ProcessNotebookData(object):
 
         processed_df = (
             processed_rdd
-            .map(lambda x: Row(x))
+            #.map(lambda x: Row(x))
             #.toDF(processed_schema)
             #.select("notebook_id", "lib_counts")
             .toDF(["notebook_id", "lib_counts"])
