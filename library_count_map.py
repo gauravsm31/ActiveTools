7e3ff73acf17074a61fee68db9bbbb5ba34b7d3e
gauravsm31 <gauravsm@uw.edu>
2019-09-25 18:09:30 -0700

    batch processing 19

--- library_count_map.py
+++ library_count_map.py
@@ -10,27 +10,6 @@ import pyspark
 from pyspark.sql.types import StringType
 import boto3
 
-def ProcessEachNotebook(notebook_url_df_row):
-    file_path = notebook_url_df_row.url
-    file_name = os.path.basename(file_path)
-    notebook_id = os.path.splitext(file_name)[0]
-
-    lines = spark.read.text(file_path).rdd.map(lambda r: r[0])
-    ls = lines.map(lambda x: x) \
-    .filter(lambda x: 'import' in x) \
-    .map(lambda x: x.split(' ')) \
-    .map(lambda x: [x[i+1] for i in range(len(x)) if x[i]=='"import' or x[i]=='"from']) \
-    .map(lambda x: x[0].split('.')).map(lambda x: x[0].split('\\')) \
-    .map(lambda x: x[0]) \
-    .map(lambda x: (x,1)) \
-    .reduceByKey(lambda n,m: n+m) \
-    .map(lambda x: x[0])
-
-    lib_count = ls.count()
-
-    print(notebook_id,lib_count)
-
-    return (notebook_id,lib_count)
 
 class ProcessNotebookData(object):
 
@@ -74,6 +53,29 @@ class ProcessNotebookData(object):
         files_urls_df = self.spark.createDataFrame(url_list_rdd, url_list_schema)
         return files_urls_df
 
+    def ProcessEachNotebook(self, notebook_url_df_row):
+        file_path = notebook_url_df_row.url
+        file_name = os.path.basename(file_path)
+        notebook_id = os.path.splitext(file_name)[0]
+
+        lines = self.spark.read.text(file_path).rdd.map(lambda r: r[0])
+        ls = lines.map(lambda x: x) \
+        .filter(lambda x: 'import' in x) \
+        .map(lambda x: x.split(' ')) \
+        .map(lambda x: [x[i+1] for i in range(len(x)) if x[i]=='"import' or x[i]=='"from']) \
+        .map(lambda x: x[0].split('.')).map(lambda x: x[0].split('\\')) \
+        .map(lambda x: x[0]) \
+        .map(lambda x: (x,1)) \
+        .reduceByKey(lambda n,m: n+m) \
+        .map(lambda x: x[0])
+
+        lib_count = ls.count()
+
+        print(notebook_id,lib_count)
+
+        return [notebook_id,lib_count]
+
+
     def NotebookMapper(self, file_list):
 
         files_urls_df = self.NotebookUrlListToDF(file_list)
@@ -81,7 +83,7 @@ class ProcessNotebookData(object):
         processed_rdd = (
             files_urls_df
             .rdd
-            .map(lambda x: ProcessEachNotebook(x))
+            .map(lambda x: self.ProcessEachNotebook(x))
         )
 
         #processed_schema = StructType([StructField("notebook_id", StringType(), False),
@@ -89,7 +91,7 @@ class ProcessNotebookData(object):
 
         processed_df = (
             processed_rdd
-            .map(lambda x: [x[0],x[1]])
+            .map(lambda x: Row(x))
             #.toDF(processed_schema)
             #.select("notebook_id", "lib_counts")
             .toDF(["notebook_id", "lib_counts"])
