a8efd02b9ee52a012f8146b658fb7e90c3023fdf
gauravsm31 <gauravsm@uw.edu>
2019-09-27 12:09:00 -0700

    batch debug day3 19

--- library_count_map.py
+++ library_count_map.py
@@ -69,21 +69,21 @@ class ProcessNotebookData(object):
         notebook_id = os.path.splitext(file_name)[0]
         print(notebook_id)
 
-        lines = self.spark.read.text(file_path).rdd.map(lambda r: r[0])
-        ls = lines.map(lambda x: x) \
-        .filter(lambda x: 'import' in x) \
-        .map(lambda x: x.split(' ')) \
-        .map(lambda x: [x[i+1] for i in range(len(x)) if x[i]=='"import' or x[i]=='"from']) \
-        .map(lambda x: x[0].split('.')).map(lambda x: x[0].split('\\')) \
-        .map(lambda x: x[0]) \
-        .map(lambda x: (x,1)) \
-        .reduceByKey(lambda n,m: n+m) \
-        .map(lambda x: ('lib',1)) \
-        .reduceByKey(lambda n,m: n+m) \
-        .map(lambda x : (notebook_id,x[1]))
+        # lines = self.spark.read.text(file_path).rdd.map(lambda r: r[0])
+        # ls = lines.map(lambda x: x) \
+        # .filter(lambda x: 'import' in x) \
+        # .map(lambda x: x.split(' ')) \
+        # .map(lambda x: [x[i+1] for i in range(len(x)) if x[i]=='"import' or x[i]=='"from']) \
+        # .map(lambda x: x[0].split('.')).map(lambda x: x[0].split('\\')) \
+        # .map(lambda x: x[0]) \
+        # .map(lambda x: (x,1)) \
+        # .reduceByKey(lambda n,m: n+m) \
+        # .map(lambda x: ('lib',1)) \
+        # .reduceByKey(lambda n,m: n+m) \
+        # .map(lambda x : (notebook_id,x[1]))
         #lib_count = ls.count()
         #self.spark.stop()
-        return ls
+        return (notebook_id,1)
 
 
     def NotebookMapper(self, file_list):
