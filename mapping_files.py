01f4c6594999abb700696eeb8ec7d3e02b39b1ec
gauravsm31 <gauravsm@uw.edu>
2019-09-28 10:23:21 -0700

    mapping files 5

--- mapping_files.py
+++ mapping_files.py
@@ -129,7 +129,8 @@ def ProcessEachFile(file_path):
 
     #file_path_list = file_path.collect()[0]
 
-    filepath = file_path[0]
+    file_path = file_path[0]
+    print("%s ..........................................." %file_path)
 
     file_name = os.path.basename(file_path)
     notebook_id = os.path.splitext(file_name)[0]
