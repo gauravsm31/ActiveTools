796ee0f1adace05dbb5540425fcca111343ca452
gauravsm31 <gauravsm@uw.edu>
2019-09-30 06:51:41 -0700

    filter data

--- mapping_files.py
+++ mapping_files.py
@@ -87,7 +87,8 @@ class ProcessNotebookData(object):
 
         print('got file df ..................................')
         # Farm out audio files to Spark workers with a map
-        processed_rdd = files_urls_df.rdd.flatMap(ProcessEachFile) #.filter(lambda x: x is not u'')
+        processed_rdd = files_urls_df.rdd.flatMap(ProcessEachFile) \
+                        .filter(lambda x: x[0] != 'nolibrary')
 
         processed_schema = StructType([StructField("library", StringType(), False),
                                          StructField("datetime", StringType(), False ),
