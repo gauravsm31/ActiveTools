187fd50a7b4e81b9a51e7cb7425b92de60d9788b
gauravsm31 <gauravsm@uw.edu>
2019-10-05 19:40:58 -0700

    Splitting Code 10

---
+++ main.py
@@ -0,0 +1,11 @@
+import ProcessNotebookData
+
+def main():
+    parent_folder = 'sample_data/data/'
+    # notebooks_folder_names must have entries of same length
+    notebooks_folder_names = ['test_notebooks_1/','test_notebooks_2/']
+    # notebooks_folder = "notebooks_1/"
+    proc = ProcessNotebookData()
+    proc.run(parent_folder,notebooks_folder_names)
+
+main()
--- mapping_files.py
+++ mapping_files.py
@@ -167,12 +167,12 @@ class ProcessNotebookData(object):
         print("Saved To Postgres .......................................")
 
 
-def main():
-    parent_folder = 'sample_data/data/'
-    # notebooks_folder_names must have entries of similar length
-    notebooks_folder_names = ['test_notebooks_1/','test_notebooks_2/']
-    # notebooks_folder = "notebooks_1/"
-    proc = ProcessNotebookData()
-    proc.run(parent_folder,notebooks_folder_names)
-
-main()
+# def main():
+#     parent_folder = 'sample_data/data/'
+#     # notebooks_folder_names must have entries of same length
+#     notebooks_folder_names = ['test_notebooks_1/','test_notebooks_2/']
+#     # notebooks_folder = "notebooks_1/"
+#     proc = ProcessNotebookData()
+#     proc.run(parent_folder,notebooks_folder_names)
+#
+# main()
